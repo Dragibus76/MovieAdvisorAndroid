a0343ab457f77e52c0cca77c958a50cb44572245
Dragibus <jeremiecool76700@hotmail.fr>
2021-12-29 15:30:07 +0100

    Build Basics + Navbar Menu

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,58 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from "react";
+import { StyleSheet, Text, View,ScrollView,FlatList } from 'react-native';
+import Colors from '../constants/Colors';
+import GenreCard from '../components/GenreCard';
+import ItemSeparator from '../components/ItemSeparator';
+import Fonts from '../constants/Fonts';
+
+const Genres = ["Tout", "Action", "Aventure", "Comédie", "Romance", "Horreur", "Science-Fiction", "Fantastique"];
+
+const HomeScreen = () => {
+  const [activeGenre, setActiveGenre] = useState("Tout")
+
+
+
+  return (
+    <ScrollView style={styles.container}>
+      <StatusBar style="auto" translucent={false} backgroundColor={Colors.BASIC_BACKGROUND} />
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerTitle}>A L'affiche</Text>
+        <Text style={styles.headerSubtitle}>VOIR TOUT</Text>
+      </View>
+      <View style={styles.genreListContainer}>
+        <FlatList data={Genres} horizontal showsHorizontalScrollIndicator={false} keyExtractor={(item) => item} ItemSeparatorComponent={() => <ItemSeparator width={20}/>} ListHeaderComponent={() => <ItemSeparator width={20}/>} ListFooterComponent={() => <ItemSeparator width={20}/>} renderItem={({item}) => 
+          <GenreCard genreName={item} active={item === activeGenre ? true : false} onPress={setActiveGenre}/>
+        }/>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.BASIC_BACKGROUND,
+  },
+  headerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  headerTitle: {
+    fontSize: 28,
+    fontFamily: Fonts.REGULAR,
+  },
+  headerSubtitle: {
+    fontSize: 13,
+    color: Colors.ACTIVE,
+    fontFamily: Fonts.BOLD,
+  },
+  genreListContainer: {
+    paddingVertical: 10,
+  },
+});
+
+export default HomeScreen;
